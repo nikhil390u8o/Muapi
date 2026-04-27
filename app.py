5f954246c6b36d1bfd9753a7327c51c76565a40c
nikhil390u8o <sangeetasharma1005sa@gmail.com>
2026-04-27 11:59:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,5 +77,5 @@ def stream():
         return {"error": str(e)}, 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 2000))
     app.run(host="0.0.0.0", port=port)
